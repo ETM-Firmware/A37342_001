ae7b9997a66fafbf9d7a3f7188d94529a1782e9b
dparkermit <dparkermit@yahoo.com>
2016-01-06 15:17:16 -0800

    91-36926-001 Rev 1
    Build For Production
    Agile Rev 1

--- A36926_001.X/FIRMWARE_VERSION.h
+++ A36926_001.X/FIRMWARE_VERSION.h
@@ -8,9 +8,9 @@
 #ifndef __FIRMWARE_VERSION_H
 #define	__FIRMWARE_VERSION_H
 
-#define FIRMWARE_AGILE_REV     0
+#define FIRMWARE_AGILE_REV     1
 #define FIRMWARE_BRANCH        0
-#define FIRMWARE_MINOR_REV     3
+#define FIRMWARE_MINOR_REV     0
 
 #endif	/* FIRMWARE_VERSION_H */
 
