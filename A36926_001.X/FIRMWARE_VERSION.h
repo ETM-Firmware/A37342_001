c0cbec569e2adfd8d815e8bac952c5d9fbe50ca8
dparkermit <dparkermit@yahoo.com>
2016-02-15 13:39:47 -0800

    A36926-001 Rev 1.0.1
    Merging Updates to Faults

--- A36926_001.X/FIRMWARE_VERSION.h
+++ A36926_001.X/FIRMWARE_VERSION.h
@@ -9,8 +9,8 @@
 #define	__FIRMWARE_VERSION_H
 
 #define FIRMWARE_AGILE_REV     1
-#define FIRMWARE_BRANCH        1
-#define FIRMWARE_MINOR_REV     3
+#define FIRMWARE_BRANCH        0
+#define FIRMWARE_MINOR_REV     1
 
 #endif	/* FIRMWARE_VERSION_H */
 
